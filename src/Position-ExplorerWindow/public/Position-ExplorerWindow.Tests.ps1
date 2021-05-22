6ad34f0080e5c462117e15bae3661de1c172b744
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-05-22 01:20:36 +0000

    Fix: Tag unit tests as 'Unit'

--- src/Position-ExplorerWindow/public/Position-ExplorerWindow.Tests.ps1
+++ src/Position-ExplorerWindow/public/Position-ExplorerWindow.Tests.ps1
@@ -2,7 +2,7 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "Position-ExplorerWindow" {
+Describe "Position-ExplorerWindow" -Tag 'Unit' {
 
     Context 'Powershell versions' {
 
